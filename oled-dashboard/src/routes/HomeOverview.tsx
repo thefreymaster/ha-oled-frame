fd9e737334846898b631b7f512d70670aa71c437
Evan Freymiller <ejf1990@gmail.com>
2026-04-13 23:07:01 -0400

    restored padding

--- oled-dashboard/src/routes/HomeOverview.tsx
+++ oled-dashboard/src/routes/HomeOverview.tsx
@@ -748,7 +748,7 @@ export function HomeOverview() {
       flexDirection={isLandscape ? "row" : "column"}
       alignItems={isLandscape ? "flex-start" : "center"}
       justifyContent={isLandscape ? "flex-start" : "space-between"}
-      p={isLandscape ? "8" : "18"}
+      p={isLandscape ? "8" : "16"}
       gap={isLandscape ? "4vmin" : "0"}
     >
       {isLandscape ? (
